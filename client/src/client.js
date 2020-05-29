dd9fe57b209865295e4d6570333507684b435ddb
Juriy Bura <juriy.bura@gmail.com>
2020-05-29 21:09:53 +0200

    grid

--- client/src/client.js
+++ client/src/client.js
@@ -16,20 +16,37 @@ const onChatSubmitted = (sock) => (e) => {
   sock.emit('message', text);
 };
 
-const drawRect = (ctx, x, y, color) => {
-  ctx.fillStyle = color;
-  ctx.fillRect(x - 10, y - 10, 20, 20);
+const drawGrid = (ctx, numCells, cellSize) => {
+
+  ctx.strokeWidth = 7;
+  ctx.strokeStyle = '#333333';
+
+  ctx.beginPath();
+
+  for (let i = 0; i < numCells + 1; i++) {
+    ctx.moveTo(cellSize*i, 0);
+    ctx.lineTo(cellSize*i, 400);
+
+    ctx.moveTo(0, cellSize*i);
+    ctx.lineTo(400, cellSize*i);
+  }
+  ctx.stroke();
 };
 
 (() => {
-  const color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-
   log('welcome');
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
 
+  const myColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+  const numCells = 20;
+  const cellSize = Math.floor(Math.min(canvas.width, canvas.height)/numCells);
+
+  drawGrid(ctx, numCells, cellSize);
   const handleClick = (x, y) => {
-    sock.emit('click', { x, y, color });
+    const cellX = Math.floor(x/cellSize);
+    const cellY = Math.floor(y/cellSize);
+    sock.emit('click', { x: cellX, y: cellY, color: myColor });
   };
 
   canvas.addEventListener('click', (e) => {
@@ -40,7 +57,10 @@ const drawRect = (ctx, x, y, color) => {
 
   const sock = io();
   sock.on('connect', () => log('connected'));
-  sock.on('click', ({ x, y, color }) => drawRect(ctx, x, y, color));
+  sock.on('click', ({ x, y, color }) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
+  });
   sock.on('message', log);
 
   document
