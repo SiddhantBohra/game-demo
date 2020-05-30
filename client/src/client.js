a2569df4e0b1990ea0996bfa044a3281d1bd8f61
Juriy Bura <juriy.bura@gmail.com>
2020-05-30 08:38:50 +0200

    basic canvas

--- client/src/client.js
+++ client/src/client.js
@@ -17,10 +17,29 @@ const onChatSubmitted = (sock) => (e) => {
   sock.emit('message', text);
 };
 
+const createBoard = (canvas) => {
+  const ctx = canvas.getContext('2d');
+
+  const fillRect = (x, y) => {
+    ctx.fillStyle = '#cc4a73';
+    ctx.fillRect(x, y, 50, 50);
+  };
+
+  return {
+    fillRect
+  };
+};
+
 (() => {
   log('welcome');
 
   const sock = io();
+  const canvas = document.querySelector('canvas');
+
+  const { fillRect } = createBoard(canvas);
+
+  fillRect(100, 100);
+
   sock.on('connect', () => log('connected'));
   sock.on('message', log);
 
